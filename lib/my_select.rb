24fa51e8edf49043d6f5e14fd7abb4a42c5f18dd
Natasha Buck <natashabuck1@gmail.com>
2018-09-26 13:35:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ meets_criteria = []
+ while i < collection.length
+  element = yield collection[i]
+  if element == true
+    meets_criteria << collection[i]
+  end
+  i += 1
+ end
+ meets_criteria
 end
